fa97a4ed136308da49cbf604d79de7b3653b7dc7
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-04 15:10:39 +0300

    Made a folder and a file

---
+++ implement stack/implement_stack_using_queues.py
@@ -0,0 +1,24 @@
+class MyStack:
+
+    def __init__(self):
+        pass
+
+    def push(self, x: int) -> None:
+        pass
+
+    def pop(self) -> int:
+        pass
+
+    def top(self) -> int:
+        pass
+
+    def empty(self) -> bool:
+        pass
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
