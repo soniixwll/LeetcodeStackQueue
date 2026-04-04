6707674033b31acef88cd531528dcc46c3518a69
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-04 14:15:59 +0300

    Added my class Stack

--- implement_queue_using_stacks.py
+++ implement_queue_using_stacks.py
@@ -1,3 +1,24 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def is_empty(self):
+        if len(self.stack) == 0:
+            return True
+        return False
+
+    def push(self, item):
+        self.stack.append(item)
+
+    def pop(self):
+        return self.stack.pop()
+
+    def peek(self):
+        return self.stack[-1]
+
+    def __len__(self):
+        return len(self.stack)
+
 class MyQueue:
 
     def __init__(self):
