c355aadd9f0bd9b131055c85c421d5353b9f5221
soniixwll <parobocha.pn@ucu.edu.ua>
2026-04-06 00:10:02 +0300

    Fixing problems: removed structures with random access to data

--- implement stack/implement_stack_using_queues.py
+++ implement stack/implement_stack_using_queues.py
@@ -1,27 +1,48 @@
-from collections import deque
+"""Implementing Stack using Queues"""
+class Node:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Queue:
     def __init__(self):
-        self.queue = deque()
+        self.queue = None
+        self.head = None
+        self._size = 0
 
     def push(self, x):
-        self.queue.append(x)
+        new_node = Node(x)
+
+        if self.head is None:
+            self.head = new_node
+            self.queue = new_node
+        else:
+            # while self.queue.next:
+            #     self.queue = self.queue.next
+            self.queue.next = new_node
+            self.queue = self.queue.next
+
+        self._size += 1
 
     def pop(self):
-        if len(self.queue) != 0:
-            return self.queue.popleft()
-        return False
+        if self.head is None:
+           self.queue = None
+        removed_thing = self.head.val
+
+        self.head = self.head.next
+
+        self._size -= 1
+
+        return removed_thing
 
     def peek(self):
-        if len(self.queue) != 0:
-            return self.queue[0]
-        return False
+        return self.head.val
 
     def size(self):
-        return len(self.queue)
+        return self._size
 
     def is_empty(self):
-        if len(self.queue) == 0:
+        if self._size == 0:
             return True
         return False
 
